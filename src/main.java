3a0c483cc0cc75fa955438182157238add7978e3
Charles Moore <moorc2@mail.uc.edu>
2026-02-09 20:56:25 -0500

    basics done

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        double springMaintenance = 307.50;
+        double summerMaintnenace = 210.32;
+        double fallMaintenance = 487.72;
+        double winterMaintenance = 101.11;
+        double totalYearlyMaintenance = 0;
+
+        totalYearlyMaintenance = springMaintenance + summerMaintnenace + fallMaintenance + winterMaintenance;
+
+        System.out.println(totalYearlyMaintenance);
+
+
+    }
+}
