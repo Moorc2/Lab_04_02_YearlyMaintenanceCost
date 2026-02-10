7a3eae4c9259abf2225e070be083c6eb7dc70082
Charles Moore <moorc2@mail.uc.edu>
2026-02-09 21:02:23 -0500

    fixed typo in fall maintenance variable

--- src/main.java
+++ src/main.java
@@ -1,15 +1,15 @@
 public class main {
     public static void main(String[] args) {
         double springMaintenance = 307.50;
-        double summerMaintnenace = 210.32;
+        double summerMaintenance = 210.32;
         double fallMaintenance = 487.72;
         double winterMaintenance = 101.11;
         double totalYearlyMaintenance = 0;
 
-        totalYearlyMaintenance = springMaintenance + summerMaintnenace + fallMaintenance + winterMaintenance;
+        totalYearlyMaintenance = springMaintenance + summerMaintenance + fallMaintenance + winterMaintenance;
 
         System.out.println("Spring Maintenance is $" + springMaintenance);
-        System.out.println("Summer Maintenance is $" + summerMaintnenace);
+        System.out.println("Summer Maintenance is $" + summerMaintenance);
         System.out.println("Fall Maintenance is $" + fallMaintenance);
         System.out.println("Winter Maintenance is $" + winterMaintenance);
         System.out.println("Total Yearly Maintenance is $" + totalYearlyMaintenance);
